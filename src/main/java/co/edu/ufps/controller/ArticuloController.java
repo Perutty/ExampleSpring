ac07351e7d82bf40675684c2cd6bf541c11905a8
Jorge Hernandez <jorgekevinhl@ufps.edu.co>
2022-07-04 18:27:57 -0500

    Finalizado el edit

--- src/main/java/co/edu/ufps/controller/ArticuloController.java
+++ src/main/java/co/edu/ufps/controller/ArticuloController.java
@@ -10,7 +10,9 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -28,12 +30,12 @@ public class ArticuloController {
 
 	@RequestMapping("/list")
 	public String listArticulos(Model model, HttpServletRequest sr) {
-		
+
 		List<Articulo> listActual = articuloService.getAll();
 		listMostrar.clear();
 		int user_id = (int) sr.getSession().getAttribute("user_id");
-		listActual.forEach((arts)->{
-			if(arts.getUsuario_id() == user_id)
+		listActual.forEach((arts) -> {
+			if (arts.getUsuario_id() == user_id)
 				listMostrar.add(arts);
 		});
 
@@ -48,17 +50,37 @@ public class ArticuloController {
 
 	@PostMapping("/save-article")
 	public String registerArticulo(Articulo article, Model model, HttpSession sesion, HttpServletRequest rq) {
-		String recortar =  (String) rq.getSession().getAttribute("pais").toString();
-		String pais_sustituto = recortar.substring(0, 3);
-		article.setPais(pais_sustituto);
+		String recortar = (String) rq.getSession().getAttribute("pais").toString();
+		article.setPais(recortar.substring(0, 3));
 		article.setUsuario_id((int) rq.getSession().getAttribute("user_id"));
-		
+
 		Long datetime = System.currentTimeMillis();
 		Timestamp timestamp = new Timestamp(datetime);
 		article.setFecha_creacion(timestamp);
-		
-		
+
 		articuloService.save(article);
 		return "redirect:/articulo/list";
+	}
+
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable("id") Integer id, Model model) {
+		articuloService.delete(id);
+		return "redirect:/articulo/list";
+	}
+
+	@GetMapping("/update/{id}")
+	public String update(@PathVariable("id") Integer id, Articulo articulo, Model model) {
+		if(id!=null){
+			model.addAttribute("editArticulo", articuloService.get(id));
+		}else{
+			model.addAttribute("articulo", new Articulo());
+		}
+		return "editarArt";
+	}
+
+	@GetMapping("/ver/{id}")
+	public String ver (@PathVariable("id") Integer id, Articulo articulo, Model model) {
+		model.addAttribute("verArticulo", articuloService.get(id));
+		return "verArticulo";	
 	}
 }
